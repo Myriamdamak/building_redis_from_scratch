c1db087d9136356a530fd769eb2c935a6a66424d
Meriem Damak <Meriemdamak@gmail.com>
2026-09-12 13:57:16 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,16 +17,17 @@ public class Main {
          serverSocket.setReuseAddress(true);
     //      // Wait for connection from client.
          clientSocket = serverSocket.accept();
+         clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
        } catch (IOException e) {
-         System.out.println("IOException: " + e.getMessage());
-       } finally {
-         try {
-           if (clientSocket != null) {
-             clientSocket.close();
-           }
-         } catch (IOException e) {
            System.out.println("IOException: " + e.getMessage());
-         }
+       } finally {
+           try {
+               if (clientSocket != null) {
+                   clientSocket.close();
+               }
+           } catch (IOException e) {
+               System.out.println("IOException: " + e.getMessage());
+           }
        }
   }
 }
